caf07aac19703bbc253eca86ecd2e01b97241884
DaoVuDat <daovudat@localhost.localdomain>
2019-12-03 17:15:08 +0700

    simple detail meal screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dummy_categories.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = "/meal-detail";
@@ -6,13 +7,33 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final meal = DUMMY_MEALS.firstWhere((element) => element.id == mealId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('$mealId'),
+        title: Text(meal.title),
       ),
-      body: Center(
-        child: Text("The meal - $mealId"),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+            height: 200,
+            width: double.infinity,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            height: 200,
+            child: ListView.builder(
+              itemBuilder: (context, index) =>
+                  Padding(
+                    padding:EdgeInsets.all(10),child: Text("${meal.ingredients[index]}")),
+              itemCount: meal.ingredients.length,
+            ),
+          )
+        ],
       ),
     );
   }
